6b090e6aa14e4dfa8b071f2d3126a24feb66b895
Vladislav <vlad.savko21@gmail.com>
2022-10-24 17:20:22 +0300

    refactoring + added events to filter

--- src/components/Photosets/index.js
+++ src/components/Photosets/index.js
@@ -1,25 +1,70 @@
 import "./index.scss"
-import { useEffect, useState, useMemo } from "react"
+import { useEffect, useState, useRef } from "react"
 import { flickrAPI } from "../../middleware/api/flickrAPI"
 import PagonationPhotosets from "./PaginationPhotosets"
 import AllPhotosets from "./AllPhotosets"
+import { useNavigate } from "react-router-dom"
+import { PhotosetsSkeleton } from "./Skeleton"
 
-const Photosets = (param) => {
+const User = (ctx) => {
     const [userInfo, setUserInfo] = useState(JSON.parse(localStorage.getItem("userInfo")))
     const [photosetsInfo, setPhotosetsInfo] = useState(JSON.parse(localStorage.getItem("photosetsInfo")))
+
     const [loading, setLoading] = useState(false)
     const [photosetsOffset, setPhotosetsOffset] = useState(null)
-    const [page, setPage] = useState(param.page)
+    const [openedPhotosetId, setOpenedPhotosetId] = useState(ctx.params.photosetId)
+
+    const [page, setPage] = useState(ctx.params.page)
     const [countPages, setCountPages] = useState(0)
+
+    const [searchOpened, setSearchOpened] = useState(false)
+    const [filterOpened, setFilterOpened] = useState(false)
+    const [customSelectOpened, setCustomSelectOpened] = useState(false)
+
+    const [filter, setFilter] = useState(JSON.parse(ctx.params.filter))
+
+    const inputSearchRef = useRef(null)
+    const inputFilterPhotosetNameRef = useRef("")
+    const inputFilterSortRef = useRef(0)
+
+    const sortObject = {
+        0: "Sort by default",
+        1: "Sort by name",
+    }
+
+    const navigate = useNavigate()
+
+    useEffect (() => {
+        let params = ctx.params
+
+        if (userInfo && userInfo.userId == params.userId) {
+            getPhotosetsInfo()
+        } else {
+            getPhotosetsInfo(params.userId)
+        }
+
+        if (page != params.page) {
+            setPage(params.page)
+        }
+
+        if (filter != JSON.parse(params.filter)) {
+            setFilter(JSON.parse(params.filter))
+        }
+
+        if (openedPhotosetId != params.photosetId) {
+            setOpenedPhotosetId(params.photosetId)
+        }
+
+    }, [ctx.params])
 
     useEffect (() => {
         if (photosetsInfo) {
             localStorage.setItem("photosetsInfo", JSON.stringify(photosetsInfo))
+            localStorage.setItem("userInfo", JSON.stringify(userInfo))
 
-            setPhotosetsOffset(getPhotosetsOffset())
-            setCountPages(Math.ceil(photosetsInfo.countPhotosets / 9))
+            getPhotosetsOffset(filter)
         }
-    }, [photosetsInfo])
+    }, [photosetsInfo, userInfo])
 
     useEffect (() => {
         if (photosetsOffset) {
@@ -27,23 +72,66 @@ const Photosets = (param) => {
         }
     }, [photosetsOffset])
 
-    useEffect (() => {
-        getPhotosetsInfo()
-    }, [param.page])
-
-    const getPhotosetsInfo = async () => {
-        await flickrAPI().getPhotosets(userInfo.userId).then((response) => {
-            setPhotosetsInfo({
-                countPhotosets: response.photosets.total,
-                photoset: response.photosets.photoset,
-            })    
+    const getPhotosetsInfo = async (userId = userInfo.userId) => {
+        await flickrAPI().getPhotosets(userId).then((response) => {
+            if (response.stat == "ok") {
+                setPhotosetsInfo({
+                    countPhotosets: response.photosets.total,
+                    photoset: response.photosets.photoset,
+                })   
+    
+                if (response.photosets.total)
+                {        
+                    setUserInfo({
+                        userName: response.photosets.photoset[0].username,
+                        userId: response.photosets.photoset[0].owner,
+                    }) 
+                }
+            }
         })    
     }
 
-    const getPhotosetsOffset = () => {
+    const getPhotosetsOffset = (filterOff) => {
         let beginSlice = (page - 1) * 9
         let endSlice = beginSlice + 9
-        let slicePhotosetArray = photosetsInfo.photoset.slice(beginSlice, endSlice)
+
+        let filterPhotosetArray = Object.assign({}, photosetsInfo)
+
+        if (filterOff) {
+            switch(filterOff.sort) {
+                case "0": {
+                    filterPhotosetArray.photoset = filterPhotosetArray.photoset.sort((firstItem, secondItem) => { 
+                        return secondItem.count_views - firstItem.count_views
+                    })
+                } break
+                
+                case "1": {
+                    filterPhotosetArray.photoset = filterPhotosetArray.photoset.sort((firstItem, secondItem) => { 
+                        if (firstItem.title._content > secondItem.title._content) {
+                            return 1
+                        } else {
+                            return -1
+                        }
+                    })
+                } break
+                
+                default: {
+                    filterPhotosetArray.photoset = filterPhotosetArray.photoset.sort((firstItem, secondItem) => { 
+                        return secondItem.count_views - firstItem.count_views
+                    })
+                } break
+            }
+
+            if (filterOff.photosetName) {
+                filterPhotosetArray.photoset = filterPhotosetArray.photoset.filter((photoset) => photoset.title._content.includes(filterOff.photosetName))
+                filterPhotosetArray.countPhotosets = filterPhotosetArray.photoset.length
+            } 
+        }
+
+        let slicePhotosetArray = filterPhotosetArray.photoset.slice(beginSlice, endSlice)
+
+        setCountPages(Math.ceil(filterPhotosetArray.countPhotosets / 9))
+        filterPhotosetArray.countPhotosets = filterPhotosetArray.photoset.length
 
         slicePhotosetArray.map((photoset) => {
             let date = new Date(photoset.date_create * 1000)
@@ -52,19 +140,111 @@ const Photosets = (param) => {
             photoset.date_create = shortDate.toString()
         })
 
-        return slicePhotosetArray
+        setTimeout(() => {
+            setPhotosetsOffset(slicePhotosetArray)
+        }, 1000)   
+    }
+
+    const getUserInfo = async () => {
+        let name = inputSearchRef.current.value
+
+        if (name) {
+            await flickrAPI().getUser(name).then((response) => {
+                if (response.stat == "ok") {
+                    setUserInfo({
+                        userName: name,
+                        userId: response.user.id,
+                    })
+
+                    navigate(`/${response.user.id}/1/${JSON.stringify(filter)}`)
+                } 
+            })
+
+            inputSearchRef.current.value = ""
+        }
+    }
+
+    const handleSearchClick = (event) => {
+        event.preventDefault()
+        let inputSearchDOMElement = document.querySelector(".input--search")
+
+        if (!searchOpened) {
+            inputSearchDOMElement.classList.add("input--search--focus")
+            setSearchOpened(true)
+        } else {
+            getUserInfo()
+
+            inputSearchDOMElement.classList.remove("input--search--focus")
+            setSearchOpened(false)
+        }
+    }
+
+    const handleFilterClick = (event) => {
+        let filterContentDOMElement = document.querySelector(".photosets__filter-content")
+
+        if (!filterOpened) {
+            filterContentDOMElement.classList.add("photosets__filter-content--active")
+            setFilterOpened(true)
+        } else {
+            filterContentDOMElement.classList.remove("photosets__filter-content--active")
+            setFilterOpened(false)
+        }
+    }
+
+    const handleSelectClick = (event) => {
+        let customSelectDOMElement = document.querySelector(".select-custom")
+        let selectListDOMElement = customSelectDOMElement.querySelector(".select-custom__list")
+
+        if (!customSelectOpened) {
+            customSelectDOMElement.classList.add("select-custom--active")
+            selectListDOMElement.classList.add("select-custom__list--active")
+            setCustomSelectOpened(true)
+        } else {
+            customSelectDOMElement.classList.remove("select-custom--active")
+            selectListDOMElement.classList.remove("select-custom__list--active")
+            setCustomSelectOpened(false)
+        }
+    }
+
+    const handleSelectOptionClick = (event, sort) => {
+        event.preventDefault()
+        
+        let customSelectTextDOMElement = document.querySelector(".select-custom__selected")
+        let selectDOMElement = document.querySelector("#filterSort")
+
+        let selectOptionDOMElement = selectDOMElement.querySelector(`option[value="${sort}"]`)
+
+        selectOptionDOMElement.selected = true
+        customSelectTextDOMElement.innerHTML = sortObject[sort]
+    }
+
+    const handleSubmitFilter = (event) => {
+        event.preventDefault()
+
+        let filter_t = {
+            sort: inputFilterSortRef.current.value,
+            photosetName: inputFilterPhotosetNameRef.current.value,
+        }
+
+        navigate(`/${userInfo.userId}/1/${JSON.stringify(filter_t)}`)
     }
 
     return(
         <div className="photosets">
             <div className="photosets__header">
                 <div className="photosets__user">
-                    <span className="photosets__user-name">{userInfo.userName}</span>
+                    <span className="photosets__user-name">{userInfo ? userInfo.userName : "User Name"}</span>
                 </div>
                 <div className="photosets__find">
                     <form className="photosets__find-form">
-                        <input type="text" className="input input__search" placeholder="Enter user name"/>
-                        <button className="button button--search" type="submit"></button>
+                        <input 
+                            type="text" 
+                            name="userName"
+                            className="input input--search" 
+                            placeholder="Enter user name" 
+                            ref={inputSearchRef}
+                        />
+                        <button className="button button--search" type="submit" onClick={(event) => handleSearchClick(event)}/>
                     </form>
                 </div>
             </div>
@@ -73,19 +253,49 @@ const Photosets = (param) => {
                     <span className="photosets__header-title-text">Photosets</span>
                 </div>
                 <div className="photosets__filter">
-
+                    <button className="button button--filter" onClick={(event) => handleFilterClick(event)}/>
                 </div>
+                <form className="photosets__filter-content">
+                    <input 
+                        type="text"
+                        name="filterName"
+                        className="input input--base" 
+                        placeholder="Enter photoset name"   
+                        ref={inputFilterPhotosetNameRef}
+                    />
+
+                    <select 
+                        name="filterSort"
+                        id="filterSort"
+                        className="select"
+                        ref={inputFilterSortRef}
+                    >
+                        <option value={0}>default</option>
+                        <option value={1}>by name</option>
+                    </select>
+
+                    <div className="select-custom" onClick={(event) => handleSelectClick(event)}>
+                        <div className="select-custom__selected">
+                            Sort by default
+                        </div>
+                        <div className="select-custom__list">
+                            <button className="button select-custom__button" onClick={(event) => handleSelectOptionClick(event, 0)}>Sort by default</button>
+                            <button className="button select-custom__button" onClick={(event) => handleSelectOptionClick(event, 1)}>Sort by name</button>
+                        </div>
+                    </div>
+
+                    <button className="button button--base ml-a" type="submit" onClick={(event) => handleSubmitFilter(event)}>Accept</button>
+                </form>
             </div>
             {
-                loading &&
-                <AllPhotosets photosetsOffset={photosetsOffset}/>
+                loading ? 
+                    <AllPhotosets photosetsOffset={photosetsOffset} openedPhotosetId={openedPhotosetId} userInfo={userInfo} filter={filter} page={page}/> : <PhotosetsSkeleton />
             }
             {
-                loading &&
-                <PagonationPhotosets page={page} countPages={countPages} setPage={setPage}/>
+                <PagonationPhotosets page={page} countPages={countPages} userId={ctx.params.userId} filter={filter}/>
             }
         </div>
     )
 }
 
-export default Photosets
+export default User
