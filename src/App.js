dbab902c55314b8d642a2772197d37fd31e10065
Vladislav <vlad.savko21@gmail.com>
2022-10-24 17:06:30 +0300

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.scss'
-import { Route, Navigate, Routes, useRoutes} from 'react-router-dom'
+import { Route, Navigate, Routes, useRoutes } from 'react-router-dom'
 import React from 'react'
 
 import Index from './pages/index'
-import PhotosetList from './pages/photosetlist'
+import User from './pages/user'
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path='/photosets/' element={<PhotosetList />}>
-          <Route path='/photosets/:number' element={<PhotosetList />} />
+        <Route path='/:user' element={<User />}>
+          <Route path=':number' element={<User />}>
+            <Route path=':filter' element={<User />}>
+              <Route path=':photoset' element={<User />} />
+            </Route>
+          </Route>
         </Route>
         <Route path='*' element={<Navigate replace to="/" />} />
       </Routes>
